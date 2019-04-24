a457348af7a3b739538ec44eba19c33a9824c635
JC Romualdo <juan.romualdo@zoro.com>
2019-04-24 10:03:43 -0500

    profile icon updates

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Profileicon.css';
 import { 
     Dropdown, 
     DropdownToggle, 
@@ -38,7 +39,7 @@ class ProfileIcon extends React.Component { //initial state of the application
                     right
                     className="b--transparent shadow-5" 
                     style={{marginTop: '20px', backgroundColor: 'rgba(255, 255, 255, 0.5)'}}>
-                    <DropdownItem>View Profile</DropdownItem>
+                    <DropdownItem onClick={() => this.props.toggleModal()}>View Profile</DropdownItem>
                     <DropdownItem onClick={() => this.props.onRouteChange('signout')}>Sign Out</DropdownItem>
                 </DropdownMenu>
             </Dropdown>
